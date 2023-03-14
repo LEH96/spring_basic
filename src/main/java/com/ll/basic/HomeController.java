588d89602fb47ff3efddf168fc7b2097fc302ae6
LEH96 <lutea67@naver.com>
2023-03-14 14:16:30 +0900

    people 추가

--- src/main/java/com/ll/basic/HomeController.java
+++ src/main/java/com/ll/basic/HomeController.java
@@ -1,15 +1,25 @@
 package com.ll.basic;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
     private int count;
+    private int peopleNum = 0;
+    List<Person> people;
 
     public HomeController() {
         this.count = -1;
+        people = new ArrayList<>();
     }
 
     @GetMapping("/home/main") //해당 요청이 들어오면(이주소로 들어오면) 아래 함수 수행
@@ -35,4 +45,32 @@ public class HomeController {
     public int showIncrease() { //메소드 반환값을 int로 해도 됨, int값이 자동으로 string화 됨
         return ++count;
     }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) { //@RequestParam는 생략가능, 순서도 상관없음
+        return a+b;            //디폴트값 지정시 누락가능 위에경우는 a에만 디폴트 지정해준것
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(String name, int age) {
+        people.add(new Person(++peopleNum, name, age));
+        return peopleNum+"번 사람을 추가되었습니다.";
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> showPeople() {
+        return people;
+    }
+}
+
+@AllArgsConstructor // 아래 모든 변수를 가진 생성자 자동생성(롬복 기능)
+@Getter
+@ToString
+class Person {
+    private int id;
+    private String name;
+    private int age;
 }
