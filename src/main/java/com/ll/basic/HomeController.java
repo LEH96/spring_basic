5b59a71b0d94ff2f295f75dfcb9b8c4be5036c68
LEH96 <lutea67@naver.com>
2023-03-14 11:22:43 +0900

    /home/main2, main3 추가

--- src/main/java/com/ll/basic/HomeController.java
+++ src/main/java/com/ll/basic/HomeController.java
@@ -9,6 +9,18 @@ public class HomeController {
     @GetMapping("/home/main") //해당 요청이 들어오면(이주소로 들어오면) 아래 함수 수행
     @ResponseBody //아래 메서드를 실행한 후 그 리턴값을 응답으로 출력해줌
     public String showMain() {
-        return "Hello";
+        return "안녕하세요.";
+    }
+
+    @GetMapping("/home/main2") //해당 요청이 들어오면(이주소로 들어오면) 아래 함수 수행
+    @ResponseBody //아래 메서드를 실행한 후 그 리턴값을 응답으로 출력해줌
+    public String showMain2() {
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3") //해당 요청이 들어오면(이주소로 들어오면) 아래 함수 수행
+    @ResponseBody //아래 메서드를 실행한 후 그 리턴값을 응답으로 출력해줌
+    public String showMain3() {
+        return "즐거웠습니다.";
     }
 }
