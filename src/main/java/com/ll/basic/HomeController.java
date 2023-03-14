26bba70a1e1946d2a49d59e37539565bb5ad6148
LEH96 <lutea67@naver.com>
2023-03-14 11:44:10 +0900

    /home/increase 추가

--- src/main/java/com/ll/basic/HomeController.java
+++ src/main/java/com/ll/basic/HomeController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    private int count;
+
+    public HomeController() {
+        this.count = -1;
+    }
+
     @GetMapping("/home/main") //해당 요청이 들어오면(이주소로 들어오면) 아래 함수 수행
     @ResponseBody //아래 메서드를 실행한 후 그 리턴값을 응답으로 출력해줌
     public String showMain() {
@@ -22,5 +28,11 @@ public class HomeController {
     @ResponseBody //아래 메서드를 실행한 후 그 리턴값을 응답으로 출력해줌
     public String showMain3() {
         return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/increase") //해당 요청이 들어오면(이주소로 들어오면) 아래 함수 수행
+    @ResponseBody //아래 메서드를 실행한 후 그 리턴값을 응답으로 출력해줌
+    public int showIncrease() { //메소드 반환값을 int로 해도 됨, int값이 자동으로 string화 됨
+        return ++count;
     }
 }
